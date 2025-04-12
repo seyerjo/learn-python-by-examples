d51cb8471465cefb473590ff340815fcfd75ddd6
seyerjo <seyerjo@msn.com>
2025-04-12 13:57:09 +0100

    Reorganize the code and improve the structure of the main function in sample_02_primitives_data_types.py

--- sample_02_primitives_data_types.py
+++ sample_02_primitives_data_types.py
@@ -1,29 +1,34 @@
 # MAIN CODE
 
-"""
-Let's look at the data types and data structures we can work with in Python.
-"""
+"""Example demonstrating various primitive data types in Python."""
 
-# Variables with data types.
-variable_integer = 1
-variable_float = 3.14
-variable_complex = 1 + 2j
-variable_string = "Hello, Coder!"
-variable_list = [1, 2, 3]
-variable_tuple = (4.0, 5.5, 6.9)
-variable_dictionary = {"key": "value"}
-variable_set = {7, 8, 9}
-variable_boolean = False
-variable_none = None
 
-# The 'type' function returns the data type.
-print(type(variable_integer))  # Integer number.
-print(type(variable_float))  # Float number.
-print(type(variable_complex))  # Complex number.
-print(type(variable_string))  # String of characters.
-print(type(variable_list))  # List of elements.
-print(type(variable_tuple))  # Tuple of elements.
-print(type(variable_dictionary))  # Dictionary of key-value pairs.
-print(type(variable_set))  # Set of unique elements.
-print(type(variable_boolean))  # Boolean value.
-print(type(variable_none))  # NoneType object.
+def main():
+    """Main function to demonstrate various primitive data types."""
+    # Variables with different data types.
+    integer_variable = 1
+    float_variable = 3.14
+    complex_variable = 1 + 2j
+    string_variable = "Hello, Coder!"
+    list_variable = [1, 2, 3]
+    tuple_variable = (4.0, 5.5, 6.9)
+    dictionary_variable = {"key": "value"}
+    set_variable = {7, 8, 9}
+    boolean_variable = False
+    none_variable = None
+
+    # Using the 'type' function to determine data types.
+    print(type(integer_variable))       # int
+    print(type(float_variable))         # float
+    print(type(complex_variable))       # complex
+    print(type(string_variable))        # str
+    print(type(list_variable))          # list
+    print(type(tuple_variable))         # tuple
+    print(type(dictionary_variable))    # dict
+    print(type(set_variable))           # set
+    print(type(boolean_variable))       # bool
+    print(type(none_variable))          # NoneType
+
+
+if __name__ == "__main__":
+    main()
